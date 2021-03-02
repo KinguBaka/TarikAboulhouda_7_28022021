6ae8ef2e1d16df9e95562c42a9b297e2a74466f8
Tarik Aboulhouda <tarikab92@yahoo.fr>
2021-03-02 12:12:54 +0100

    finition route login, signup, profil & regex/token

---
+++ back/routes/messageCtrl.js
@@ -0,0 +1,20 @@
+// Imports
+const models = require('../models');
+const asyncLib = require('async');
+
+// Constants
+
+
+// Routes
+
+module.exports = {
+
+    createMessage: (req, res) => {
+        
+    },
+
+    listMessage: (req, res) => {
+        
+    }
+
+};
